4492325d61b9113a2abffae6073906d7d775f106
MRyzhylo <playa77777@gmail.com>
2019-09-20 15:43:09 +0300

    complete post and get functions

--- handler.js
+++ handler.js
@@ -1,20 +1,20 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-const db = AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10"});
-const uuid = require('uuid');
+const db = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+const uuid = require('uuid/v4');
 
 const postsTable = process.env.POSTS_TABLE;
 
-function responce (statusCode, message) {
+function response(statusCode, message) {
   return {
     statusCode: statusCode,
     body: JSON.stringify(message)
-  }
+  };
 }
 
-function sortByDate (a,b) {
-  if(a.createdAt > b.createdAt) {
+function sortByDate(a, b) {
+  if (a.createdAt > b.createdAt) {
     return -1;
   } else return 1;
 }
@@ -24,23 +24,32 @@ module.exports.createPost = (event, context, callback) => {
 
   const post = {
     id: uuid(),
-    createdAt: new Date(),
+    createdAt: new Date().toISOString(),
     userId: 1,
-    title: reqBoby.title,
+    title: reqBody.title,
     body: reqBody.body
-  }
-  return db.put({
-    TadleName: postsTable,
-    Item: post
-  }).promise().then(() => {
-    callback(null, responce(201,post))
-  }).catch(err => responce(null, responce(err.statusCode, err)))
-}
+  };
 
+  return db
+    .put({
+      TableName: postsTable,
+      Item: post
+    })
+    .promise()
+    .then(() => {
+      callback(null, response(201, post));
+    })
+    .catch((err) => response(null, response(err.statusCode, err)));
+};
+// Get all posts
 module.exports.getAllPosts = (event, context, callback) => {
-  return db.scan({
-    TadleName: postsTable
-  }).promise().then(res => {
-    callback(null, responce(200, res.Item.sort(sortByDate)))
-  }).catch(err => responce(null, responce(err.statusCode, err)))
-}
+  return db
+    .scan({
+      TableName: postsTable
+    })
+    .promise()
+    .then((res) => {
+      callback(null, response(200, res.Items.sort(sortByDate)));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
